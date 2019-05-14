173a081f89a73994995a0a92e78c82f9621c66ca
Nassim <nassim.morouche1@gmail.com>
2019-05-14 17:35:44 +0200

    Changed textColor of showInformationsActivity

--- app/src/main/java/com/maruani/games/mylittleheroesgi/views/ShowInformationsActivity.java
+++ app/src/main/java/com/maruani/games/mylittleheroesgi/views/ShowInformationsActivity.java
@@ -1,6 +1,7 @@
 package com.maruani.games.mylittleheroesgi.views;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
